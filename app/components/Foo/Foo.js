5a73739dda4c65c5e29768696d2176be09bdc148
claudiocarcaci <claudiocarcaci@gmail.com>
2018-12-26 21:26:41 +0100

    Restart project

--- app/components/Foo/Foo.js
+++ app/components/Foo/Foo.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Container, Button, Text } from 'native-base';
 
 export class Foo extends Component {
-  render() {
-    return (
-			<Container>
+    render() {
+        return (
+		<Container>
 				<Button>
 					<Text>Button</Text>
 				</Button>
 			</Container>
-   	);
-	}
+   	    );
+    }
 }
