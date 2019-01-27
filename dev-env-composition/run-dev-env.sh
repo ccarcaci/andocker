34d5507090b93147df1325f9e391706e7db0ce53
claudiocarcaci <claudiocarcaci@gmail.com>
2019-01-27 19:05:39 +0100

    Folder organization review

--- dev-env-composition/run-dev-env.sh
+++ dev-env-composition/run-dev-env.sh
@@ -3,9 +3,8 @@
 export butomoVersion=8.1
 yes | docker system prune
 
-
-docker-compose build --build-arg butomoVersion=$butomoVersion
+docker-compose build --build-arg butomoVersion=$butomoVersion android-emulator
 docker-compose up -d
 exec google-chrome --new-window http://localhost:6080 > /dev/null &
 
-echo "Press F5 con Chrome (localhost:6080) when android-emulator has been done to view the emulator"
+echo "To view the emulator press F5 on Chrome (localhost:6080) when android-emulator service is loaded"
