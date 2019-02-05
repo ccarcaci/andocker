155ccbdd708247c3a21910872037fadb19bc1ba0
Claudio Carcaci <claudio.carcaci@mia-platform.eu>
2019-02-05 11:49:03 +0100

    Removed dockerfile version of ecosystem

--- dev-env-full-docker-compose/compose.sh
+++ dev-env-full-docker-compose/compose.sh
@@ -8,6 +8,5 @@ yes | docker system prune
 docker-compose build --build-arg butomoVersion=$butomoVersion emulator expo
 docker-compose up -d
 
-# exec google-chrome --new-window http://localhost:6080 > /dev/null &
-
+exec google-chrome --new-window http://localhost:6080 > /dev/null &
 echo "To view the emulator press F5 on Chrome (localhost:6080) when emulator service is loaded"
