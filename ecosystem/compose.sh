d7bc07e5c0f822762a9c2b74bbbfe7d5b449defb
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-09-24 22:25:31 +0200

    Run dockerize within run script Run google-chrome commented out

--- ecosystem/compose.sh
+++ ecosystem/compose.sh
@@ -4,8 +4,7 @@ export butomoVersion=8.1
 
 docker-compose down
 yes | docker system prune
-
 docker-compose build --build-arg butomoVersion=$butomoVersion emulator expo
-docker-compose up -d
+docker-compose up
 
-exec google-chrome --new-window http://localhost:6080 > /dev/null &
+# exec google-chrome --new-window http://localhost:6080 > /dev/null &
