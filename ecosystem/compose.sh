d15098f9fa2a59b6e9de61ea22857170461c9d5a
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-12-01 18:39:43 +0100

    WIP
    Fix users within Dockerfile
    Fix paths
    Move npm install comands into compose.sh (run in host system)
    Update docker-android image version
    Use Samsung Galaxy S10

--- ecosystem/compose.sh
+++ ecosystem/compose.sh
@@ -2,7 +2,8 @@
 
 docker-compose down
 yes | docker system prune
-docker-compose build emulator expo
+docker image rm -f ecosystem_expo:latest
+(cd ../code; npm i; npm audit fix)
 docker-compose up
 
 # exec google-chrome --new-window http://localhost:6080 > /dev/null &
