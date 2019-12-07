202e3291ae4c9685a0c7e74142b61657c72d7262
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-12-07 20:59:40 +0100

    Remove android and ios build folders Update dependencies Add some useful log lines to run script

--- ecosystem/run-expo-app.sh
+++ ecosystem/run-expo-app.sh
@@ -1,2 +1,6 @@
 (cd /home/node; ./dockerize -wait tcp://emulator:5555; ./platform-tools/adb connect emulator:5555)
+echo "Waiting 60 second for Android to startup"
+echo "Open you browser to http://localhost:6080 to open emulator"
+sleep 60
+echo "Waiting... done"
 (cd code; npm run android)
