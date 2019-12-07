f216cf5165269925146a64f2cf18c2ec78e7d6c4
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-12-07 18:28:45 +0100

    Remove note about unmaintained project Update React Native project with last expo-cli experience

--- code/App.js
+++ code/App.js
@@ -1,34 +1,38 @@
-import React, { Component } from "react";
-import Starter from "./app/Starter";
-import { KeepAwake } from "expo";
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
 
 export default class App extends Component {
-	constructor() {
-		super();
-		this.state = { 
-			isReady: false
-		};
-	}
-
-	componentWillMount() {
-		this.loadFonts();
-	}
-
-	async loadFonts() {
-		await Expo.Font.loadAsync({
-			Roboto: require("native-base/Fonts/Roboto.ttf"),
-			Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-		});
-		
-		this.setState({ isReady: true });
-	}
-	
-	render() {
-		<KeepAwake/>
-		if(this.state.isReady) {
-			return <Starter/>;
-		} else {
-			return <Expo.AppLoading/>;
-		}
-	};
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
