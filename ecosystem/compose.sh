5d90e7bd64adb7360432c476e01ae06ba96579e4
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-12-08 10:57:37 +0100

    Remove unused react-native dependencies Remove npm audit fix, it updates some dependencies that create a version mismatch Use Samsung Galaxy S10 as device Fix typo

--- ecosystem/compose.sh
+++ ecosystem/compose.sh
@@ -5,5 +5,5 @@ if [ "$1" == "--rebuild-ecosystem" ]; then
   docker image rm -f ecosystem_expo:latest
 fi
 
-(cd ../code; rm -rf .expo; rm -rf node_modules; npm i; npm audit fix)
+(cd ../code; rm -rf .expo; rm -rf node_modules; npm i)
 docker-compose up
